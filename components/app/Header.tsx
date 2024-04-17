21723428fa3972f26026908e66190f8bc9876804
Jim <mrjim.development@gmail.com>
2024-04-17 21:53:17 +0800

    fix: signout function

--- components/app/Header.tsx
+++ components/app/Header.tsx
@@ -140,24 +140,21 @@ function Header({ user }: { user: User }) {
                   </Menu.Item>
                 </div>
                 <div className="py-1">
-                  <form method="POST" action="#">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          type="submit"
-                          className={classNames(
-                            active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                            "block w-full px-4 py-2 text-left text-sm"
-                          )}
-                          onClick={async () => {
-                            await signOut();
-                          }}
-                        >
-                          Sign out
-                        </button>
-                      )}
-                    </Menu.Item>
-                  </form>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={classNames(
+                          active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                          "block w-full px-4 py-2 text-left text-sm"
+                        )}
+                        onClick={async () => {
+                          await signOut();
+                        }}
+                      >
+                        Sign out
+                      </button>
+                    )}
+                  </Menu.Item>
                 </div>
               </Menu.Items>
             </Transition>
