6469121a26aa3088d5fd3961de5658c248d8a99d
Mark Kenneth Badilla <markkenneth.badilla@cit.edu>
2024-04-18 10:17:22 +0800

    fix: refactor UserActionButton component to use correct type for ref props

--- components/app/user_action_button.tsx
+++ components/app/user_action_button.tsx
@@ -77,7 +77,7 @@ export default function UserActionButton({ selectedUser }: { selectedUser: User 
       <Menu
         as="div"
         className="relative inline-block text-left"
-        ref={setReferenceElement}
+        ref={setReferenceElement as unknown as React.RefObject<HTMLElement> | null}
       >
         <div>
           <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
@@ -97,7 +97,7 @@ export default function UserActionButton({ selectedUser }: { selectedUser: User 
         >
           <Menu.Items
             className="absolute right-0 z-50 mt-2 w-56 origin-top-left divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-            ref={setPopperElement}
+            ref={setPopperElement as React.Ref<HTMLDivElement> | null}
             style={styles.popper}
           >
             <div className="py-1">
