8588670f667634234fbf0ee62fcf6f378d005c6d
Bianca Jessa A. Carabio <biancajessa.carabio@cit.edu>
2024-04-20 17:01:17 +0800

    feat(Organization): Modify Organizations Table Colors

--- components/app/organization_options.tsx
+++ components/app/organization_options.tsx
@@ -1,12 +1,9 @@
 "use client";
+import { deleteUser, sendPasswordRecovery } from "@/lib/userActions";
 import { Dialog, Menu, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
 import { ChevronDownIcon, TrashIcon, UserIcon } from "@heroicons/react/20/solid";
-import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import JSONPretty from "react-json-pretty";
-import { sendPasswordRecovery, deleteUser } from "@/lib/userActions";
-import { User } from "@supabase/supabase-js";
+import { EnvelopeIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Fragment, useState } from "react";
 import Swal from "sweetalert2";
 
 const jsonTheme = {
@@ -59,7 +56,7 @@ export default function OrganizationOptions({ selectedOrg }: { selectedOrg: any 
     <>
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-charleston px-3 py-2 text-sm font-semibold text-light shadow-sm ring-1 ring-inset ring-[#525252] hover:bg-raisinblack">
             Options
             <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
           </Menu.Button>
@@ -74,20 +71,20 @@ export default function OrganizationOptions({ selectedOrg }: { selectedOrg: any 
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="fixed right-[80px] z-[100] mt-2 w-56 origin-top-left divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="fixed right-[80px] z-[100] mt-2 w-56 origin-top-left divide-y divide-[#525252] rounded-md bg-charleston shadow-lg ring-1 ring-charleston ring-opacity-5 focus:outline-none">
             <div className="py-1">
               <Menu.Item>
                 {({ active }) => (
                   <a
                     href="#"
                     className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      active ? "bg-raisinblack text-light" : "text-light",
                       "group flex items-center px-4 py-2 text-sm"
                     )}
                     onClick={() => setOpen(true)}
                   >
                     <UserIcon
-                      className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                      className="mr-3 h-5 w-5 text-light group-hover:text-light"
                       aria-hidden="true"
                     />
                     View User Info
@@ -98,13 +95,13 @@ export default function OrganizationOptions({ selectedOrg }: { selectedOrg: any 
                 {({ active }) => (
                   <a
                     className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      active ? "bg-raisinblack text-light" : "text-light",
                       "group flex items-center px-4 py-2 text-sm"
                     )}
                     href={`/organization/edit/${selectedOrg.slug}`}
                   >
                     <EnvelopeIcon
-                      className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                      className="mr-3 h-5 w-5 text-light group-hover:text-light"
                       aria-hidden="true"
                       onClick={async () => {
                         const response = await sendPasswordRecovery(selectedUser.id!);
@@ -136,13 +133,13 @@ export default function OrganizationOptions({ selectedOrg }: { selectedOrg: any 
                   <a
                     href="#"
                     className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      active ? "bg-raisinblack text-light" : "text-light",
                       "group flex items-center px-4 py-2 text-sm"
                     )}
                     onClick={deleteBtn}
                   >
                     <TrashIcon
-                      className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                      className="mr-3 h-5 w-5 text-light group-hover:text-light"
                       aria-hidden="true"
                     />
                     Delete
