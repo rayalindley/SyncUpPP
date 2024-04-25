67bd677a69bc25d75854616d2252e239612b9e80
alizamayyy <alizabataluna1@gmail.com>
2024-04-25 20:11:37 +0800

    feature(frontend): added general channel permissions component

---
+++ components/GeneralChannelPermissions.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Permission from "./Permission";
+
+export default function GeneralChannelPermissions() {
+  return (
+    <div>
+      <p className="mb-3">General Channel Permissions</p>
+      <Permission />
+    </div>
+  );
+}
