2d983491da7ead6880fa336dc96af01380d42909
Mark Kenneth S. Badilla <iammkb2002@gmail.com>
2024-05-16 15:56:02 +0800

    feat: reworked newsletter page

--- lib/newsletterActions.ts
+++ lib/newsletterActions.ts
@@ -1,83 +1,155 @@
 // lib/newsletterActions.js
 "use server";
 import { createClient } from "@/lib/supabase/server";
-import { Resend } from "resend";
+import { Resend } from 'resend';
 
-export async function fetchMembers(slug) {
-  const supabase = createClient();
+// Function to send a newsletter
+export async function sendEmail(emailContent) {
+  const resend = new Resend();
+  console.log(emailContent)
   try {
-    // First, get the organization ID based on the slug
-    const { data: organization, error: orgError } = await supabase
-      .from("organizations")
-      .select("organizationid")
-      .eq("slug", slug)
-      .single();
+    const response = await resend.emails.send({
+      from: emailContent.from,
+      to: emailContent.to,
+      subject: emailContent.subject,
+      html: emailContent.html,
+      attachments: emailContent.attachments,
+    });
 
-    if (orgError) throw orgError;
+    if (response.error) throw response.error;
 
-    // Then, get the member IDs from the organizationmembers table
-    const { data: memberIDs, error: memberIDsError } = await supabase
-      .from("organizationmembers")
-      .select("userid")
-      .eq("organizationid", organization.organizationid);
-
-    if (memberIDsError) throw memberIDsError;
-
-    // Fetch user profiles for each member ID
-    const memberProfiles = [];
-    for (const member of memberIDs) {
-      const { data: userProfile, error: userProfileError } = await supabase
-        .from("combined_user_data")
-        .select("*") // Select only required fields
-        .eq("id", member.userid)
-        .single();
-
-      if (userProfileError) throw userProfileError;
-
-      // Construct the full name
-      const fullName = `${userProfile.first_name} ${userProfile.last_name}`;
-      memberProfiles.push({ ...userProfile, name: fullName });
-    }
-
-    return memberProfiles;
-  } catch (error: any) {
-    console.error("Error:", error.message);
-  }
-}
-
-export async function getOrganizationNameBySlug(slug: string) {
-  const supabase = createClient();
-  try {
-    let { data, error } = await supabase
-      .from('organizations')
-      .select('name')
-      .eq('slug', slug)
-      .single();
-
-    if (error) {
-      console.error('Error:', error);
-      return { name: null, error: { message: error.message } };
-    } else {
-      return { name: data.name, error: null };
-    }
-  } catch (e: any) {
-    console.error('Unexpected error:', e);
     return {
-      name: null,
-      error: { message: e.message || 'An unexpected error occurred' },
+      success: true,
+      data: response.data, // Assuming the API returns a success message or email ID
+    };
+  } catch (error) {
+    console.error("Error sending newsletter:", error.message);
+    return {
+      success: false,
+      message: error.message || "Failed to send newsletter",
     };
   }
 }
 
-export async function sendNewsletter(emailContent) {
-  const resend = new Resend(process.env.RESEND_API_KEY);
-  const { data, error } = await resend.emails.send(emailContent);
+// Function to fetch sent emails
+export async function fetchSentEmails() {
+  const supabase = createClient();
+  try {
+    const { data: sentEmails, error } = await supabase
+      .from("emails")
+      .select("*"); // Select only required fields
 
-  if (error) {
-    return console.error({ error });
+    if (error) throw error;
+
+    return sentEmails;
+  } catch (error) {
+    console.error("Error fetching sent emails:", error.message);
+    return [];
   }
-  
-  return { data };
+}
 
-  // console.log("sendnewsletter", { data });
+export async function sendNewsletter(subject, content, allUsers, attachments, from) {
+  allUsers.map(async (user) => {
+
+    if (user && user.email) {
+      const emailContent = {
+        from: `${from} <onboarding@resend.dev>`,
+        to: user.email,
+        subject: subject,
+        html: content,
+        attachments: attachments, // Assuming attachments are handled elsewhere
+      };
+
+      const resppnse = await sendEmail(emailContent);
+    }
+  });
+}
+/////////////////////////////////////
+////////////////////////////////////
+// Function to fetch all members managed by the current admin
+export async function fetchMembersByAdmin(adminUuid) {
+  // //console.log('Fetching members by admin:', adminUuid);
+  const supabase = createClient();
+  try {
+    const { data: members, error } = await supabase.rpc("get_all_combined_user_data_by_admin", { admin_uuid: adminUuid });
+
+    if (error) throw error;
+
+    //console.log('Fetched members:', members);
+    return members.map((member) => ({
+      ...member,
+      name: `${member.first_name} ${member.last_name}`,
+    }));
+  } catch (error) {
+    console.error("Error fetching members by admin:", error.message);
+    return [];
+  }
+}
+
+// Function to fetch all organizations managed by the current admin
+export async function fetchOrganizationsByAdmin(adminUuid) {
+  //console.log('Fetching organizations by admin:', adminUuid);
+  const supabase = createClient();
+  try {
+    const { data: organizations, error } = await supabase.rpc("get_all_organizations_by_admin", { admin_uuid: adminUuid });
+
+    if (error) throw error;
+
+    //console.log('Fetched organizations:', organizations);
+    return organizations;
+  } catch (error) {
+    console.error("Error fetching organizations by admin:", error.message);
+    return [];
+  }
+}
+
+// Function to fetch all members of a specific organization
+export async function fetchMembersByOrganization(organizationUuid) {
+  //console.log('Fetching members by organization:', organizationUuid);
+  const supabase = createClient();
+  try {
+    const { data: members, error } = await supabase.rpc("get_all_combined_user_data_by_org", { organization_uuid: organizationUuid });
+
+    if (error) throw error;
+
+    //console.log('Fetched members:', members);
+    return members;
+  } catch (error) {
+    console.error("Error fetching members by organization:", error.message);
+    return [];
+  }
+}
+
+// Function to fetch all events managed by the current admin
+export async function fetchEventsByAdmin(adminUuid) {
+  //console.log('Fetching events by admin:', adminUuid);
+  const supabase = createClient();
+  try {
+    const { data: events, error } = await supabase.rpc("get_all_events_by_admin", { admin_uuid: adminUuid });
+
+    if (error) throw error;
+
+    //console.log('Fetched events:', events);
+    return events;
+  } catch (error) {
+    console.error("Error fetching events by admin:", error.message);
+    return [];
+  }
+}
+
+// Function to fetch all members registered for a specific event
+export async function fetchMembersByEvent(eventUuid) {
+  //console.log('Fetching members by event:', eventUuid);
+  const supabase = createClient();
+  try {
+    const { data: members, error } = await supabase.rpc("get_all_combined_user_data_by_event", { event_uuid: eventUuid });
+
+    if (error) throw error;
+
+    //console.log('Fetched members:', members);
+    return members;
+  } catch (error) {
+    console.error("Error fetching members by event:", error.message);
+    return [];
+  }
 }
