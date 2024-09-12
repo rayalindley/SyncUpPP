b6768b8021a6df3358401886bf98662617dd9c43
MrJim Development <mrjim.development@gmail.com>
2024-09-12 16:40:05 +0800

    isdatevalid function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// Function to validate date
+export function isDateValid(dateString: string): boolean {
+  const date = new Date(dateString);
+  const today = new Date();
+  const age = today.getFullYear() - date.getFullYear();
+  const monthDiff = today.getMonth() - date.getMonth();
+  const dayDiff = today.getDate() - date.getDate();
+
+  // Check if the date is valid and the user is at least 18 years old
+  if (
+    date instanceof Date &&
+    !isNaN(date.getTime()) &&
+    (age > 18 || (age === 18 && (monthDiff > 0 || (monthDiff === 0 && dayDiff >= 0))))
+  ) {
+    return true;
+  }
+  return false;
 }
