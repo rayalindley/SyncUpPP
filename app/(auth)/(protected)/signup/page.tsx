83c71042cb86f905b670643d829b870cb3373234
Aliza Bataluna <alizabataluna1@gmail.com>
2024-09-20 19:49:18 +0800

    added visibility toggle for password (signin and signup)

--- app/(auth)/(protected)/signup/page.tsx
+++ app/(auth)/(protected)/signup/page.tsx
@@ -1,6 +1,12 @@
-import { signInWith, signUp } from "@/lib/auth";
+"use client";
+
+import { signUp, signInWithGoogleAction } from "@/lib/auth";
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline'; // Add Heroicons import
+import { useState } from "react";
 
 export default function SignUp({ searchParams }: { searchParams: any }) {
+  const [showPassword, setShowPassword] = useState(false); // Add state for password visibility
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center bg-eerieblack py-12 sm:px-6 lg:px-8">
@@ -90,15 +96,26 @@ export default function SignUp({ searchParams }: { searchParams: any }) {
                 >
                   Password
                 </label>
-                <div className="mt-2">
+                <div className="mt-2 relative"> {/* Add relative positioning */}
                   <input
                     id="password"
                     name="password"
-                    type="password"
+                    type={showPassword ? "text" : "password"} // Toggle password visibility
                     autoComplete="current-password"
                     required
                     className="block w-full rounded-md border-0 bg-charleston py-1.5 text-light shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
                   />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)} // Toggle function
+                    className="absolute inset-y-0 right-0 flex items-center pr-3 text-sm text-gray-500" // Change text-gray-500 to match border color
+                  >
+                    {showPassword ? (
+                      <EyeSlashIcon className="h-5 w-5 text-gray-300" aria-hidden="true" /> // Set color to match border
+                    ) : (
+                      <EyeIcon className="h-5 w-5 text-gray-300" aria-hidden="true" /> // Set color to match border
+                    )} {/* Eye icon for password visibility */}
+                  </button>
                 </div>
               </div>
 
@@ -138,13 +155,9 @@ export default function SignUp({ searchParams }: { searchParams: any }) {
                 </div>
               </div>
 
-              <form className="mt-6 grid gap-4">
+              <form className="mt-6 grid gap-4" action={signInWithGoogleAction}>
                 <button
                   type="submit"
-                  formAction={async () => {
-                    "use server";
-                    await signInWith("google");
-                  }}
                   className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
                 >
                   <svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 24 24">
@@ -167,29 +180,6 @@ export default function SignUp({ searchParams }: { searchParams: any }) {
                   </svg>
                   <span className="text-sm font-semibold leading-6">Google</span>
                 </button>
-
-                {/* <button
-                  type="submit"
-                  formAction={async () => {
-                    "use server";
-                    await signInWith("github");
-                  }}
-                  className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
-                >
-                  <svg
-                    className="h-5 w-5 fill-[#24292F]"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <span className="text-sm font-semibold leading-6">GitHub</span>
-                </button> */}
               </form>
             </div>
           </div>
