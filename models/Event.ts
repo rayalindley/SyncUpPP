800de735d95c397b937017507c83fb98e4f2936d
Chaz Zyl G. Llaguno <chazzyl.llaguno@gmail.com>
2026-04-12 14:11:54 +0800

    fix: Toast error because of the toggle.

--- models/Event.ts
+++ models/Event.ts
@@ -1,7 +1,7 @@
 // @/models/Event.ts
 export class Event {
   constructor(
-    public eventid: string,
+    public id: string,
     public title: string,
     public description: string,
     public starteventdatetime: Date,
